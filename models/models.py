db6a378f1cbc1125a1bc7c8c2999aa8f9972d344
Andrey Sugonyaev <34348146+Andre6o6@users.noreply.github.com>
2019-05-27 18:31:52 +0300

    Update models.py

--- models/models.py
+++ models/models.py
@@ -7,37 +7,37 @@ class ForkNet(torch.nn.Module):
         self.num_anchors = num_anchors
         
         self.encoder = resnet34(pretrained=True)
-		self.pool = nn.AdaptiveAvgPool2d((S, S))
+        self.pool = nn.AdaptiveAvgPool2d((S, S))
         
         self.reg_head = nn.Conv2d(512, (4+1)*num_anchors, 1)
-		
+
         self.class_head = nn.Sequential(
             nn.Conv2d(512, num_anchors*num_features, 1),
             nn.GroupNorm(num_anchors, num_anchors*num_features), 
             nn.ReLU(inplace=True)
         )
-    
+
     def forward(self, x):
         x = self.encoder.conv1(x)
         x = self.encoder.bn1(x)
         x = self.encoder.relu(x)
         x = self.encoder.maxpool(x)
-        
+
         x = self.encoder.layer1(x) 
         x = self.encoder.layer2(x)        
         x = self.encoder.layer3(x)        
         x = self.encoder.layer4(x)
-        
+
         x = self.pool(x)
-        
-        x_reg = self.reg_head(x)		#Regression head
-        x_class = self.class_head(x)	#Classification head
-		
-		#Reshape bbox attributes
-		nB, _, nH, nW = x_reg.shape
-		x_reg = x_reg.view(nB, self.num_anchors, 4+1, nH, nW)      
-		x_reg = x_reg.permute(0, 1, 3, 4, 2)
-                
+
+        x_reg = self.reg_head(x)        #Regression head
+        x_class = self.class_head(x)    #Classification head
+
+        #Reshape bbox attributes
+        nB, _, nH, nW = x_reg.shape
+        x_reg = x_reg.view(nB, self.num_anchors, 4+1, nH, nW)      
+        x_reg = x_reg.permute(0, 1, 3, 4, 2)
+
         return x_reg, x_class
 		
 class Classifier(torch.nn.Module):
